013015326eb75044192d06c5ac2937c23d71d91b
Eric Drake <eric.drake2011@gmail.com>
2023-03-07 12:37:33 -0600

    imported components and data service

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { createBrowserRouter, RouterProvider} from "react-router-dom";
 
 import Root from "./components/Root";
+import CategoryView from "./components/CategoryView";
+import HomePage from "./components/HomePage";
+import ItemDataService from "./services/item.js";
 
 function App() {
   return (

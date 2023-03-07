2a69928163d829b424bafda1687c67c7b2b58a1d
Eric Drake <eric.drake2011@gmail.com>
2023-03-07 12:46:51 -0600

    added states, browserRouter, and paths

--- src/App.js
+++ src/App.js
@@ -7,6 +7,42 @@ import HomePage from "./components/HomePage";
 import ItemDataService from "./services/item.js";
 
 function App() {
+  const [cart, setCart] = React.useState(["Test", "Items"])
+  const [animals, setAnimals] = React.useState(["Test", "Animals"])
+  const [sketches, setSketches] = React.useState(["Test", "Sketches"])
+  const [celebrities, setCelebrities] = React.useState(["Test", "Celebrities"])
+  // Disabled until client gets commision work
+  // const [commissions, setCommissions] = React.useState(["Test", "Commissions"])
+
+  const commonProps = {}
+  const categories = {animals, sketches, celebrities}
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          path: "/", 
+          element: <HomePage />,
+        },
+        {
+          path: "/animals",
+          element: <CategoryView category={animals} />,
+        },
+        {
+          path: "/sketches",
+          element: <CategoryView category={sketches} />,
+        },
+        {
+          path: "/celebrities",
+          element: <CategoryView category={sketches} />,
+        },
+      ]
+    }
+  ])
+
+
   return (
     <div className="App">
       

4500305698207bad5a09dbab6cf6e171fefbd52e
Eric Drake <eric.drake2011@gmail.com>
2023-03-09 10:42:49 -0600

    rendered works section based on number of categories and added physical examples picture

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,6 +5,7 @@ import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 
 import logo from "../images/logo-nobg.png"
+import physicalExamples from "../images/physical-examples.png";
 
 
 
@@ -43,38 +44,102 @@ export default function HomePage({categories}) {
                         </div>
                     </div>
                 </div>
-                <h1 className="pt-5">my works</h1>
+                <h1 id="works" className="pt-5">my works</h1>
                 <div className="container border">
+                    <div className="container d-flex justify-content-between py-5">
+                        <div className="col-5 align-self-center">
+                            <img src={physicalExamples} alt="physical-examples" className="w-100"/>
+                        </div>
+                        <div className="col-5">
+                            <h2>All of my work is available for purchase in the following formats:</h2>
+                            <ul>
+                                <li><h3>William Turner Paper</h3></li>
+                                <p>A traditional mold made watercolor paper that feautures a distinct, highlytexture surface. The natural white cotton fiber paper has a uniquely toothy feel and surface texture.</p>
+                                
+                                <div className="mb-3">
+                                    <p>Available Sizes:</p>
+                                    <ul>
+                                        <li>5" x 7"</li>
+                                        <li>9" x 12"</li>
+                                        <li>11" x 18"</li>
+                                    </ul>
+                                </div>
+
+                                <li><h3>Canvas Board Print</h3></li>
+                                <p>An acid-free, lignin-free heavyweight cotton- poly blend, the elegantly tectured canvas features a bright white point, exceptionally high Dmax and wide color gamut.</p>
+
+                                <div className="mb-3">
+                                    <p>Available Sizes:</p>
+                                    <ul>
+                                        <li>5" x 7"</li>
+                                        <li>9" x 12"</li>
+                                        <li>11" x 18"</li>
+                                    </ul>
+                                </div>
+                            </ul>
+                        </div>
+                    </div>
+
                     {categories.map((category) => {
-                        return(
-                            <div className="container d-flex justify-content-around my-5">
-                                <div className="col-4 border">
+                        return( categories.indexOf(category) % 2 === 0 ? (
+                            <div key={categories.indexOf(category)} className="container d-flex justify-content-around my-5">
+                                <div className="col-4 border d-flex flex-column justify-content-center">
                                     <h3>{category[1]}</h3>
                                     <p>Brief description about category</p>
                                     <a href="/animals">View All</a>
                                 </div>
-                                <div id="carouselExampleControls" className="carousel slide col-6 border" data-bs-ride="carousel">
-                                    <div className="carousel-inner">
+                                <div id={"category" + categories.indexOf(category) + "carouselControl"} className="carousel slide col-6 border" data-bs-ride="carousel">
+                                    <div className="carousel-inner bg-dark">
                                         <div className="carousel-item active">
-                                        <img src="..." className="d-block w-100" alt="image" />
+                                        <img src="https://picsum.photos/200/300" className=" d-block m-auto" alt="preview" />
                                         </div>
                                         <div className="carousel-item">
-                                        <img src="..." className="d-block w-100" alt="image" />
+                                        <img src="https://picsum.photos/200/300" className=" d-block m-auto" alt="preview" />
                                         </div>
                                         <div className="carousel-item">
-                                        <img src="..." className="d-block w-100" alt="image" />
+                                        <img src="https://picsum.photos/200/300" className=" d-block m-auto" alt="preview" />
                                         </div>
                                     </div>
-                                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                                    <button className="carousel-control-prev" type="button" data-bs-target={"#category" + categories.indexOf(category) + "carouselControl"} data-bs-slide="prev">
                                         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                                         <span className="visually-hidden">Previous</span>
                                     </button>
-                                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                                    <button className="carousel-control-next" type="button" data-bs-target={"#category" + categories.indexOf(category) + "carouselControl"} data-bs-slide="next">
                                         <span className="carousel-control-next-icon" aria-hidden="true"></span>
                                         <span className="visually-hidden">Next</span>
                                     </button>
                                 </div>
                             </div>
+                            ) : (
+                                <div key={categories.indexOf(category)} className="container d-flex justify-content-around my-5">
+                                    <div id={"category" + categories.indexOf(category) + "carouselControl"} className="carousel slide col-6 border" data-bs-ride="carousel">
+                                        <div className="carousel-inner bg-dark">
+                                            <div className="carousel-item active">
+                                                <img src="https://picsum.photos/200/300" className=" d-block m-auto" alt="preview" />
+                                            </div>
+                                            <div className="carousel-item">
+                                                <img src="https://picsum.photos/200/300" className=" d-block m-auto" alt="preview" />
+                                            </div>
+                                            <div className="carousel-item">
+                                                <img src="https://picsum.photos/200/300" className=" d-block m-auto" alt="preview" />
+                                            </div>
+                                        </div>
+                                        <button className="carousel-control-prev" type="button" data-bs-target={"#category" + categories.indexOf(category) + "carouselControl"} data-bs-slide="prev">
+                                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span className="visually-hidden">Previous</span>
+                                        </button>
+                                        <button className="carousel-control-next" type="button" data-bs-target={"#category" + categories.indexOf(category) + "carouselControl"} data-bs-slide="next">
+                                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span className="visually-hidden">Next</span>
+                                        </button>
+                                    </div>
+                                    <div className="col-4 border d-flex flex-column align-items-end justify-content-center">
+                                        <h3>{category[1]}</h3>
+                                        <p>Brief description about category</p>
+                                        <a href="/animals">View All</a>
+                                    </div>
+                                </div>
+                            )
                         )
                     })}
                 </div>

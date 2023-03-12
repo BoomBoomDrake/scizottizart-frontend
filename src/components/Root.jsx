19cf440c7ee178640f6311a9780cf6105956d87d
Eric Drake <eric.drake2011@gmail.com>
2023-03-11 22:37:28 -0600

    fixed navbar collapse styling

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -6,41 +6,42 @@ import ContactForm from "./ContactForm";
 
 export default function Root() {
 
-    // To enable scrolling to href coming from a page other than HomePage
-    const fromNavBarState = {fromNavBar: true};
+    const navBarCollapseRef = React.useRef();
+
+    const [collapse, setCollapse] = React.useState();
 
     return (
         <React.Fragment>
-            <nav className="navbar navbar-expand-lg fixed-top pt-5 pb-3" style={{backgroundColor: "#f6e7d8"}}>
+            <nav className="navbar navbar-expand-lg fixed-top pt-4" style={{backgroundColor: "#f6e7d8"}}>
                 <div className="container p-0">
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    {/*span pushes navbar-toggler to right on collapse*/}
+                    <span className="nav-brand"></span>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div className="navbar-nav container justify-content-between px-5">
-                            <Link 
-                                className="nav-link p-0 align-self-end"
-                                to={{ pathname: "/"}}
-                                state={{ref: "home"}}
-                            >
-                                Home
-                            </Link>
-                            <Link 
-                                className="nav-link p-0 align-self-end"
-                                to={{ pathname: "/", hash: "#works",}}
-                                state={{ref: "works"}}
-                            >
-                                My Works
-                            </Link>
-                            <Link 
-                                className="nav-link p-0 align-self-end"
-                                to={{ pathname: "/", hash: "#about",}}
-                                state={{ref: "about"}}
-                            >
-                                About Me
-                            </Link>
-                            <ContactForm />
-                        </div>
+                    <div ref={navBarCollapseRef} className="navbar-nav container navbar-collapse collapse justify-content-between px-5 pt-3" id="navbarCollapse">
+                        <Link
+                            className="nav-link p-0 align-self-end"
+                            to={{ pathname: "/"}}
+                            state={{ref: "home"}}
+                        >
+                            Home    
+                        </Link>
+                        <Link
+                            className="nav-link p-0 align-self-end"
+                            to={{ pathname: "/", hash: "#works",}}
+                            state={{ref: "works"}}
+                        >
+                            My Works
+                        </Link>
+                        <Link
+                            className="nav-link p-0 align-self-end"
+                            to={{ pathname: "/", hash: "#about",}}
+                            state={{ref: "about"}}
+                        >
+                            About Me
+                        </Link>
+                        <ContactForm />
                     </div>
                 </div>
             </nav>

4e8f787441899a06a4ee700cce3f162b729712aa
Eric Drake <eric.drake2011@gmail.com>
2023-03-12 14:13:07 -0500

    added skeleton of createCheckoutSession

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Root from "./components/Root";
 import CategoryView from "./components/CategoryView";
 import HomePage from "./components/HomePage";
 import ContactForm from "./components/ContactForm";
+import ShoppingCart from "./components/Shopping Cart";
 import ItemDataService from "./services/item.js";
 
 function App() {
-  const [cart, setCart] = React.useState(["Test", "Items"])
+  const [cart, setCart] = React.useState([])
   const [animals, setAnimals] = React.useState([])
   const [sketches, setSketches] = React.useState([])
   const [celebrities, setCelebrities] = React.useState([])
-  const [isLoading, setIsLoading] = React.useState(false);
   // Disabled until client gets commision work
   // const [commissions, setCommissions] = React.useState(["Test", "Commissions"])
 
@@ -26,12 +26,10 @@ function App() {
   const categories = [animals, sketches, celebrities]
 
   const getAnimals = () => {
-    setIsLoading(true);
     ItemDataService.getItemsByCategory("Animals")
       .then(response => {
         setAnimals(response.data.items)
         console.log(animals)
-        setIsLoading(false);
       })
       .catch((e) => {
         console.error(e);
@@ -39,11 +37,9 @@ function App() {
   }
 
   const getSketches = () => {
-    setIsLoading(true);
     ItemDataService.getItemsByCategory("Sketches")
       .then(response => {
         setSketches(response.data.items)
-        setIsLoading(false);
       })
       .catch((e) => {
         console.error(e);
@@ -51,16 +47,18 @@ function App() {
   }
 
   const getCelebrities = () => {
-    setIsLoading(true);
     ItemDataService.getItemsByCategory("Celebrities")
       .then(response => {
         setCelebrities(response.data.items)
         console.log(celebrities);
-        setIsLoading(false);
       })
       .catch((e) => {
         console.error(e);
       });
+  }
+
+  const createCheckoutSession = (arr) => {
+    console.log(arr);
   }
 
   const router = createBrowserRouter([
@@ -70,7 +68,7 @@ function App() {
       children: [
         {
           path: "/",
-          element: <HomePage animals={animals} categories={categories} isLoading={isLoading}/>,
+          element: <HomePage categories={categories}/>,
         },
         {
           path: "/animals",
@@ -93,6 +91,12 @@ function App() {
     <React.Fragment>
       <RouterProvider router={router} />
       <ContactForm />
+      <ShoppingCart 
+        cart={cart} 
+        setCart={setCart} 
+        animals={animals}
+        createCheckoutSession={createCheckoutSession}
+      />
     </React.Fragment>
   );
 }

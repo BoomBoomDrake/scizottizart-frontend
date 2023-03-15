9323a5b79913fb6db59faa7a56fb63d52b8b3831
Eric Drake <eric.drake2011@gmail.com>
2023-03-15 14:51:46 -0500

    passed addToCart to ItemCard via props

--- src/components/CategoryView.jsx
+++ src/components/CategoryView.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 
+import ItemCard from "./ItemCard";
+
 export default function CategoryView(props) {
+    
+    const targetRef = React.useRef();
+
+    React.useEffect(() => {
+        scroll();
+    })
+
+    // Ensures navbar doesn't overlap page content
+    const scroll = () => {
+        setTimeout(() => {
+            targetRef.current.scrollIntoView({
+                behavior: "smooth"
+            })
+        }, 100)
+    }
+    
     return (
-        <img src={props.category[0].img} alt="test" />
+        <div className="container p-0">
+            <span ref={targetRef} className="spacer d-block"></span>
+            <h1>{props.category[0].category}</h1>
+            <div className="container border border-2 border-dark pb-5 mb-5">
+                {props.category.map((obj) => {
+                    return(
+                        <ItemCard item={obj} addToCart={props.addToCart}/>
+                    )
+                })}
+            </div>
+        </div>
     )
 }

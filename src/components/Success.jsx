8a047672bb95b1ae9a7b36ca7126a469cf69ce04
Eric Drake <eric.drake2011@gmail.com>
2023-05-03 13:39:58 -0500

    redirected window back to homepage after timeout

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 
+import loader from "../images/spinning-circles.svg"
+
 export default function Success() {
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            window.location.replace("http://localhost:3000/");
+        }, 5000)
+    })
+
     return (
         <React.Fragment>
             <span className="spacer d-block"></span>
             <div className="container border border-2 border-dark pb-5 mb-5 text-center">
                 <h2>Success!</h2>
-                <p>Thank you for your purchase. Please</p>
+                <p>Thank you for your purchase. Rerouting back to homepage.</p>
+                <img src={loader} alt="spinning circles loader" />
             </div>
         </React.Fragment>  
     )

5854c408c19621a2fbd7e19683ef888a48067110
Eric Drake <eric.drake2011@gmail.com>
2023-05-03 13:43:15 -0500

    redirected back to homepage after timeout

--- src/components/Cancel.jsx
+++ src/components/Cancel.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 
+import loader from "../images/spinning-circles.svg";
+
 export default function Cancel() {
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            window.location.replace("http://scizottizart.com/");
+        }, 5000)
+    })
+
     return (
         <React.Fragment>
-            <div className="container border border-2 border-dark pb-5 mb-5">
-                <h1>Cancel</h1>
+            <span className="spacer d-block"></span>
+            <div className="container border border-2 border-dark pb-5 mb-5 text-center">
+                <h2>Transaction Cancelled!</h2>
+                <p>Rerouting back to homepage.</p>
+                <img src={loader} alt="spinning circles loader" />
             </div>
-        </React.Fragment>
+        </React.Fragment> 
     )
 }
